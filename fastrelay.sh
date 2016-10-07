1c1c709527421776c93e21e9bdb1d6508d6e1ea2
Beard <security@kernel.is>
2016-10-07 00:48:07 -0700

    Create fastrelay.sh

---
+++ fastrelay.sh
@@ -0,0 +1,83 @@
+#!/bin/bash -
+#title			:fastrelay.sh
+#description		:This script will make it super easy to run a Tor Relay Node.
+#author			:TorWorld A Project Under The CryptoWorld Foundation.
+#contributors		:KsaRedFx
+#date			:10-06-2016
+#version		:0.0.2 Alpha
+#os			:Debian/Jessie
+#usuage			:bash fastrelay.sh
+#notes			:If you have any problems feel free to email us: security[at]torworld.org
+#===============================================================================================================================================
+
+# Getting Codename of the OS
+flavor=`lsb_release -cs`
+
+# Installing dependencies for Tor
+read -p "We need to get the dependecies for Tor (y,n) " REPLY
+
+if [ "${REPLY,,}" == "y" ]; then
+
+   echo deb http://deb.torproject.org/torproject.org $flavor main >> /etc/apt/sources.list.d/torproject.list
+
+   echo deb-src http://deb.torproject.org/torproject.org $flavor main >> /etc/apt/sources.list.d/torproject.list
+
+   gpg --keyserver keys.gnupg.net --recv 886DDD89
+
+   gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | apt-key add -
+
+fi
+
+# Updating / Upgrading System
+read -p "We need to update/upgrade the system (y,n ) " REPLY
+
+if [ "${REPLY,,}" == "y" ]; then
+
+   apt-get update
+
+   apt-get dist-upgrade
+
+fi
+
+# Installing Tor
+read -p "Do you want to install Tor? (MAKE SURE YOU'RE 100% SURE ABOUT THIS! (y,n) " REPLY
+
+if [ "${REPLY,,}" == "y" ]; then
+
+   apt-get install tor
+   
+   echo "Getting status of Tor.."
+   
+   service tor status
+   
+   echo "Stopping Tor service..."
+   
+   service tor stop
+
+fi
+
+# Customizing Torrc to suit Relay
+# Nickname for Relay
+read -p "Enter your desired Nickname for your Relay: "  Name
+
+echo "Nickname $Name" > /etc/tor/torrc
+
+# DirPort for Relay
+read -p "Enter what port number you want DirPort to look at: " DirPort
+echo "DirPort $DirPort" >> /etc/tor/torrc
+
+# ORPort for Relay
+read -p "Enter what port number you want ORPort to look at: " ORPort
+echo "ORPort $ORPort" >> /etc/tor/torrc
+
+# Exit Policy for Relay
+echo "By default we do not allow exit policies for Relays (So this content is static.)"
+echo "Exitpolicy reject *:*" >> /etc/tor/torrc
+
+# Contact Info for Relay
+read -p "Enter your contact info for your Relay: " Info
+echo "ContactInfo $Info" >> /etc/tor/torrc
+
+# Restarting Tor service
+echo "Restarting the Tor service..."
+service tor restart
