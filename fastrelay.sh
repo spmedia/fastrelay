21a1639449dab9ea690359d7a8cc4d2c3a3a9ed3
Beard <security@kernel.is>
2016-10-08 07:50:02 -0700

    Update fastrelay.sh

--- fastrelay.sh
+++ fastrelay.sh
@@ -1,5 +1,21 @@
 #!/bin/bash -
-#title			:fastrelay.sh
+#===============================================================================================================================================
+# (C) Copyright 2016 TorWorld (https://torworld.org/) a project under the CryptoWorld Foundation (https://cryptoworld.is).
+#
+# Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.gnu.org/licenses/gpl-3.0.en.html
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#===============================================================================================================================================
+#title			:FastRelay
 #description		:This script will make it super easy to run a Tor Relay Node.
 #author			:TorWorld A Project Under The CryptoWorld Foundation.
 #contributors		:KsaRedFx
